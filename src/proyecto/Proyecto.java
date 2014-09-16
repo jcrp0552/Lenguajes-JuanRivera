97fda02232dc6215a34be3147c3b4cc197d6dead
jcrp0552 <jcrp0552@hotmail.com>
2014-09-15 19:41:43 -0500

    Update Proyecto.java

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -16,7 +16,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      System.out.println("mi primer programa feo");
+      System.out.println("mi primer programa cambio2");
     }
     
 }
